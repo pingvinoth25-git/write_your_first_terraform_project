38a8606993f5fc4a1966a0e94f2188adb3bcf0b9
pingvinoth25-git <pingvinoth25@gmail.com>
2025-11-26 09:39:46 +0530

    Update main1.tf

--- aws/local_state/main1.tf
+++ aws/local_state/main1.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-demo-bucket-12345"   # must be globally unique
+
+  tags = {
+    Name        = "MyTerraformS3Bucket"
+    Environment = "Dev"
+  }
+}
 
